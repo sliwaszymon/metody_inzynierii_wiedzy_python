cc04f64d42e677c675e86353660ec51b6b748a5e
Szymon Sliwa <szymon242820@gmail.com>
2022-03-16 14:38:23 +0100

    Update euclid.py

--- euclid.py
+++ euclid.py
@@ -14,23 +14,26 @@ def euclidean_distance(list1, list2):
     else:
         return False
 
-def distances(macierz):
-    ans = {}
-    for x in range(len(macierz)):
-        if macierz[x][-1] not in ans.keys():
-            ans[int(macierz[x][-1])] = [euclidean_distance(macierz[0], macierz[x])]
-        else:
-            ans[macierz[x][-1]].append(euclidean_distance(macierz[0], macierz[x]))
-    return ans
 
-def distance_between(x, lista):
-    ans = {}
-    for y in range(len(lista)):
-        if lista[y][-1] not in ans.keys():
-            ans[int(lista[y][-1])] = [euclidean_distance(x, lista[y])]
-        else:
-            ans[lista[y][-1]].append(euclidean_distance(x, lista[y]))
-    return ans
+# PRACA DOMOWA 1
+# def distances(macierz):
+#     ans = {}
+#     for x in range(1, len(macierz), 1):
+#         if macierz[x][-1] not in ans.keys():
+#             ans[int(macierz[x][-1])] = [euclidean_distance(macierz[0], macierz[x])]
+#         else:
+#             ans[macierz[x][-1]].append(euclidean_distance(macierz[0], macierz[x]))
+#     return ans
+
+# NIBY DOBRZE ALE ZAMIAST DO SLOWNIKA MAJA BYC TUPLE
+# def distance_between(x, lista):
+#     ans = {}
+#     for y in range(len(lista)):
+#         if lista[y][-1] not in ans.keys():
+#             ans[int(lista[y][-1])] = [euclidean_distance(x, lista[y])]
+#         else:
+#             ans[lista[y][-1]].append(euclidean_distance(x, lista[y]))
+#     return ans
 
 def _distance_between(x, lista):
     ans = []
@@ -47,16 +50,13 @@ def _grupujemy(lista, n):
             ans[x[0]].append(x[1])
     for key in ans.keys():
         ans[key].sort()
-        temp = 0
-        for x in range(n):
-            temp += ans[key][x]
-        ans[key] = temp
+        ans[key] = sum(ans[key][:n])
     return ans        
     
     
 
 macierz = get_from_file("australian.dat")
-dystanse = distances(macierz)
+# dystanse = distances(macierz)
 # print(dystanse)
 
 x = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
