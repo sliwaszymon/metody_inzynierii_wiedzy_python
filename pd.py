25b63b13bcafe2b4b929c54445f3b5ead315c8de
Szymon Śliwa <szymon242820@gmail.com>
2022-04-01 14:43:45 +0200

    pd for 06.04

--- pd.py
+++ pd.py
@@ -1,6 +1,9 @@
 import random as rand
 import numpy as np
 
+#############################################
+# na 30.03.2022
+#############################################
 def sqrt(value, epsilon):
     a, b = value, 1
     while abs(a-b) > epsilon:
@@ -17,7 +20,6 @@ def montecarlo_integration(foo, xp, xk, sample):
         if foo(i) > maximum:
             maximum = foo(i)
     minimum, maximum = minimum+10, maximum+10
-
     points = 0
     for j in range(sample):
         x = rand.uniform(xp, xk)
@@ -32,3 +34,33 @@ def foo(x):
     return (x**3+ 3*x**2)/3   # to jest całka powyzszej
 
 print(montecarlo_integration(foo, 4, 7, 1000000))
+
+#############################################
+# na 06.04.2022
+#############################################
+# WERSJA EASY
+# def mean_average(list1):
+#     sum = 0
+#     for x in list1:
+#         sum += x
+#     return x / len(list1)
+
+# def variance(list1):
+#     mean_avg = mean_average(list1)
+#     sum = 0
+#     for x in list1:
+#         sum += pow((x-mean_avg),2)
+#     return sum / len(list1)
+
+# def standard_deviation(list1):
+#     return pow(variance(list1), 1/2)
+
+# WERSJA HARD
+def mean_average(list1):
+    return sum(list1) / len(list1)
+
+def variance(list1):
+    return sum([pow((x-mean_average(list1)), 2) for x in list1]) / len(list1)
+
+def standard_deviation(list1):
+    return pow(variance(list1), 1/2)
