2745198a1bb0713a8770306b1dd9a1b737017807
Szymon Śliwa <szymon242820@gmail.com>
2022-04-06 23:00:00 +0200

    linear regression?

--- matrix_utils.py
+++ matrix_utils.py
@@ -1,3 +1,6 @@
+from statistics import linear_regression
+import numpy as np
+
 class MatrixUtils:
     @staticmethod
     def transpose(matrix:list):
@@ -9,19 +12,18 @@ class MatrixUtils:
 
     @staticmethod
     def det(matrix:list):
-        if len(matrix) == len(matrix[0]) and len(matrix) > 0:
-            if len(matrix) == 2:
+        if len(matrix) == 2:
                 return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-            else:
-                matrixes = {x:[] for x in range(len(matrix))}
-                temp = matrix[1:]
-                for x in range(len(matrixes.keys())):
-                    for y in range(len(temp)):
-                        matrixes[x].append(temp[y][:x] + temp[y][x+1:])
-                ans = 0
-                for x in range(len(matrixes.values())):
-                    ans += matrix[0][x] * pow(-1, 2+x) * MatrixUtils.det(list(matrixes.values())[x])
-                return ans
+        elif len(matrix) == len(matrix[0]) and len(matrix) > 0:
+            matrixes = {x:[] for x in range(len(matrix))}
+            temp = matrix[1:]
+            for x in range(len(matrixes.keys())):
+                for y in range(len(temp)):
+                    matrixes[x].append(temp[y][:x] + temp[y][x+1:])
+            ans = 0
+            for x in range(len(matrixes.values())):
+                ans += matrix[0][x] * pow(-1, 2+x) * MatrixUtils.det(list(matrixes.values())[x])
+            return ans
         else:
             # raise ValueError("dimension mismatched")
             return None
@@ -60,15 +62,27 @@ class MatrixUtils:
     
     @staticmethod
     def inverse(matrix:list):
-        if len(matrix) == len(matrix[0]):
-            det = MatrixUtils.det(matrix)
-            if det == 0:
-                return None
-            else:
+        det = MatrixUtils.det(matrix)
+        if det == 0:
+            return None
+        else:
+            if len(matrix) == 2:
+                return [[matrix[1][1]/det, (-1)*matrix[0][1]/det], [(-1)*matrix[1][0]/det, matrix[0][0]/det]]
+            elif len(matrix) == len(matrix[0]):
                 transpose = MatrixUtils.transpose(MatrixUtils.algebraic_complements(matrix))
                 return [[float(y/det) for y in range(len(transpose[x]))] for x in range(len(transpose))]
-        else:
-            return None
+            else:
+                return None
+    
+    @staticmethod
+    def linear_regression(matrix:list):
+        x = [matrix[x][0] for x in range(len(matrix))]
+        y = [matrix[x][1] for x in range(len(matrix))]
+        X = [[1 for x in range(len(x))], x]
+        dot1 = np.dot(X, MatrixUtils.transpose(X))
+        inverse = MatrixUtils.inverse([list(x) for x in list(dot1)])
+        dot2 = np.dot(y, MatrixUtils.transpose(X))
+        return list(np.dot(inverse, dot2))
 
 
 class Matrix:
@@ -78,6 +92,7 @@ class Matrix:
     transposed:list = None
     algebraic_complements:list = None
     inverse:list = None
+    linear_regression:list = None
 
     def __init__(self, matrix:list):
         self.value = matrix
@@ -86,6 +101,7 @@ class Matrix:
         self.transposed = MatrixUtils.transpose(self.value)
         self.algebraic_complements = MatrixUtils.algebraic_complements(self.value)
         self.inverse = MatrixUtils.inverse(self.value)
+        self.linear_regression = MatrixUtils.linear_regression(self.value)
     
     def __str__(self):
         return str({
@@ -94,15 +110,10 @@ class Matrix:
             'det': self.det,
             'transposed': self.transposed,
             'algebraic complements': self.algebraic_complements,
-            'inverse': self.inverse
+            'inverse': self.inverse,
+            'linear_regression': self.linear_regression
         })
 
 
-mat = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-
-matrix = Matrix(mat)
-print(matrix)
+mat = Matrix([[2,1],[5,2],[7,3],[8,3]])
+print(mat)
